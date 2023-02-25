8d5b2ef11643b180a0370d8683603f9334e01b17
Open-Assistant-Ai <125748156+Open-Assistant-Ai@users.noreply.github.com>
2023-02-25 09:58:43 -0500

    Add files via upload

---
+++ divi_up.sh
@@ -0,0 +1,20 @@
+cd ~/Git/Open-Assistant
+
+# For datacollection
+rm -rf /ansible
+rm -rf /deploy
+rm -rf /docs
+rm -rf /notebooks
+rm -rf /.git
+rm -rf /.github
+
+
+# For Infrence
+# rm -rf /ansible
+# rm -rf /deploy
+# rm -rf /discord-bot
+# rm -rf /docs
+# rm -rf /notebooks
+# rm -rf /website
+# rm -rf /.git
+# rm -rf /.github
---
+++ setup.sh
@@ -0,0 +1,40 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y git curl python3 python3-pip nodejs npm
+
+pip3 install requests torch datasets transformers nltk
+
+cd ~
+
+mkdir Git
+
+cd Git
+
+git clone https://github.com/Open-Assistant-Ai/base-models
+
+git clone https://github.com/Open-Assistant-Ai/datasets
+
+git clone https://github.com/LAION-AI/Open-Assistant
+
+git clone https://github.com/huggingface/text-generation-inference
+
+git clone https://github.com/huggingface/transformers
+
+git clone https://github.com/LAION-AI/Open-Instruction-Generalist
+
+cd ~/Downloads
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker $USER
+
+curl -fsSL https://go.microsoft.com/fwlink/?LinkID=760868 -o code.deb
+sudo apt install ./<file>.deb
+
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt clean
+
+cd ~/Desktop
+
+# make a custom file i]with information in it for the user
+
+# sudo reboot now
